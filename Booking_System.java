93c4e47fc0e547673bd77ec5ffd6e744e5f9c162
Muinul@UM <149593054+muinul23@users.noreply.github.com>
2024-12-18 11:41:31 +0800

    Delete Booking_System.java

--- Booking_System.java
+++
@@ -1,6 +0,0 @@
-//the payment (just write a method for user to choose how can they pay for the tickets), such as Visa
-//optional--Database techs
-//user search the flight by Date
-
-public class Booking_System{
-}
